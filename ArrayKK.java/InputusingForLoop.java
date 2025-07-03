20d389dd18d8a7fb183b2e1161888b2aac7a592e
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-07-03 19:16:32 +0530

    using toString to print my array

--- ArrayKK.java/InputusingForLoop.java
+++ ArrayKK.java/InputusingForLoop.java
@@ -13,10 +13,11 @@ public class InputusingForLoop {
         // for(int i=0;i<arr.length;i++) {
         //     System.out.println(arr[i] + " ");
 
-        for(int num : arr) { // for every element in array, print the element
-            System.out.print(num + " "); // here num represents element of the array
-        }
+        // for(int num : arr) { // for every element in array, print the element
+        //     System.out.print(num + " "); // here num represents element of the array
+        System.out.println(Arrays.toString(arr));
     }
 }
+
     
 
