e7c8f87239fd7fe12859d0bd3a3ab99dfadf404f
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-07-21 20:37:02 +0530

    Search Insert Position

---
+++ Binary_Search/Binary_Search_Questions/LCsearchInsertPosition.java
@@ -0,0 +1,24 @@
+public class LCsearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 2;
+        System.out.println(searchIndex(nums, target));
+
+    }
+    public static int searchIndex(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1; 
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+    
+}
