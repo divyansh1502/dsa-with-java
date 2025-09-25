7749fdd6cad9ba65f682dd059533aed70743fc58
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-09-25 22:10:13 +0530

    Recursion Lec 2

---
+++ Recursion/Lec-02-Level1/sumofNnumbers.java
@@ -0,0 +1,12 @@
+public class sumofNnumbers {
+    public static void main(String[] args) {
+        int ans = sum(6);
+        System.out.println(ans);
+    }
+    static int sum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+}
