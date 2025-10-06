2d878bbcadf673aca63e51c5bcc7706e587cecba
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-06 10:28:19 +0530

    Permutations

---
+++ Recursion/Lec-06-Permutations/ArrayListPermutation.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class ArrayListPermutation {
+    public static void main(String[] args) {
+        ArrayList<String> ans = permutationsList("", "abc");
+        System.out.println(ans);
+    }
+    static ArrayList<String> permutationsList(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        //local tp this call
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
+        }
+        return ans;
+    }
+}
