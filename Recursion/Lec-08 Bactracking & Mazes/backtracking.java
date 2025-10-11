b82e33762d7f7af47ffdef4fa391bcd04006d933
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-11 21:12:05 +0530

    Backtracking & Mazes Completed

---
+++ Recursion/Lec-08 Bactracking & Mazes/backtracking.java
@@ -0,0 +1,42 @@
+public class backtracking {
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+        backtracking("", board, 0, 0);
+    }
+    static void backtracking(String p, boolean[][] maze,  int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+        if (!maze[r][c]) {
+            return;
+        }
+
+        //I am considering this block in my path
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            backtracking(p + 'D', maze, r + 1, c);
+        }
+        if (c < maze[0].length - 1) {
+            backtracking(p + 'R', maze, r, c + 1);
+        }
+        if (r > 0) {
+            backtracking(p + 'U', maze, r - 1, c);
+        }
+        if (c > 0) {
+            backtracking(p + 'L', maze, r, c - 1);
+        }
+
+        // this line is where the function will over
+        // so before the fn gets removed, also remove the changes that were made by that fn
+        maze[r][c] = true;
+
+    }
+}
+
+
