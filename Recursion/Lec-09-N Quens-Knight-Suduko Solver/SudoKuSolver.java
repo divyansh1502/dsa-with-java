3255967bcffa6f0d9a0c925d4b6e6899941df1cd
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-14 15:35:28 +0530

    Sudoku

---
+++ Recursion/Lec-09-N Quens-Knight-Suduko Solver/SudoKuSolver.java
@@ -0,0 +1,96 @@
+public class SudoKuSolver {
+    public static void main(String[] args) {
+        int[][] board = {
+            {5,3,0,0,7,0,0,0,0},
+            {6,0,0,1,9,5,0,0,0},
+            {0,9,8,0,0,0,0,6,0},
+            {8,0,0,0,6,0,0,0,3},
+            {4,0,0,8,0,3,0,0,1},
+            {7,0,0,0,2,0,0,0,6},
+            {0,6,0,0,0,0,2,8,0},
+            {0,0,0,4,1,9,0,0,5},
+            {0,0,0,0,8,0,0,7,9}
+        };
+
+        if (solve(board)) {
+            display(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+        boolean emptyLeft = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    emptyLeft = true;
+                    break;
+                }
+            }
+            if (emptyLeft) {
+                break;
+            }
+        }
+
+        if (!emptyLeft) {
+            return true;
+        }
+
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (solve(board)) {
+                    return true;
+                } else {
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void display(int[][] board) {
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        int n = board.length;
+
+        for (int i = 0; i < n; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int sqrt = (int) Math.sqrt(n);
+        int boxRowStart = row - row % sqrt;
+        int boxColStart = col - col % sqrt;
+
+        for (int r = boxRowStart; r < boxRowStart + sqrt; r++) {
+            for (int c = boxColStart; c < boxColStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
