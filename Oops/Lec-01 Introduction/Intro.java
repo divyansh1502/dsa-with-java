a6cff9683e481289132834761bdb013bc0d0ac8b
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-17 12:07:58 +0530

    opps intro

---
+++ Oops/Lec-01 Introduction/Intro.java
@@ -0,0 +1,31 @@
+package Oops.Lec-01 Introduction;
+
+public class Intro {
+    public static void main(String[] args) {
+        // Stores 5 roll nos.
+        int[] rollnos = new int[5];
+
+        // store 5 names
+        String names = new String[5];
+
+        // data of 5 students: {roll no, name, marks}
+        int rno = new int[5];
+        Strinig name = new String[5];
+        float[] marks = new float[5];
+
+        Students[] students = new Students[5];
+
+        Student divyansh;
+    }
+    // Create a class
+    class Students {
+        int rno = new int[5];
+        Strinig name = new String[5];
+        float[] marks = new float[5];
+    }
+}
+// Class is name group of property & fn
+// A class is an template of an objects
+// An object is an instsance of a class
+// Class --> logical Construct
+// Object --> physical reality / Occupy space in memory
