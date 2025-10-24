c12b2695d708e34f3d909aacff3e143a80bafe6a
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-24 10:29:27 +0530

    Oops

--- Oops/Lec-01 Introduction/Intro.java
+++ Oops/Lec-01 Introduction/Intro.java
@@ -1,27 +1,29 @@
-package Oops.Lec-01 Introduction;
-
+import java.util.Arrays;
 public class Intro {
     public static void main(String[] args) {
         // Stores 5 roll nos.
         int[] rollnos = new int[5];
 
         // store 5 names
-        String names = new String[5];
+        String[] names = new String [5];
 
         // data of 5 students: {roll no, name, marks}
-        int rno = new int[5];
-        Strinig name = new String[5];
+        int[] rno = new int[5];
+        String[] name = new String[5];
         float[] marks = new float[5];
 
-        Students[] students = new Students[5];
+        Student[] students = new Student[5];
 
+        // just declaring
         Student divyansh;
+
+        System.out.println(Arrays.toString(students));
     }
-    // Create a class
-    class Students {
-        int rno = new int[5];
-        Strinig name = new String[5];
-        float[] marks = new float[5];
+    // Creating a class
+    class Student {
+        int rno;
+        String name;
+        float marks;
     }
 }
 // Class is name group of property & fn.
